538666bd618ea44e6f0e85d28a7003b124cd5e48
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-08-29 16:28:54 +0800

    adapter: Route rule changes

--- tests/uploadAndPlayBag.spec.ts
+++ tests/uploadAndPlayBag.spec.ts
@@ -4,83 +4,66 @@ const cases = [
   {
     name: "OSS - bz2 bag",
     url: "oss://coscene-playground/A - 上线必须回归文件集/压缩-bz2文件/GS_2023-01-04-20-26-19_103.bag",
-    noDir: false,
   },
   {
     name: "OSS - lz4 bag",
     url: "oss://coscene-playground/A - 上线必须回归文件集/压缩-lz4文件/GS_2022-12-16-21-43-31_271.bag",
-    noDir: false,
   },
   {
     name: "OSS - GSbag.active",
     url: "oss://coscene-playground/A - 上线必须回归文件集/单个active/2022-09-20-14-12-54.bag.active",
-    noDir: false,
   },
   {
     name: "OSS - KNbag.active",
     url: "oss://coscene-playground/A - 上线必须回归文件集/KNbag/",
-    noDir: false,
   },
   {
     name: "OSS - Gaoxian map",
     url: "oss://coscene-playground/A - 上线必须回归文件集/导入地图/GS地图导入/导入地图1/",
-    noDir: false,
   },
   {
     name: "OSS - Gaoxian TF",
     url: "oss://coscene-playground/A - 上线必须回归文件集/导入静态TF/",
-    noDir: false,
   },
   {
     name: "OSS - KN map",
     url: "oss://coscene-playground/A - 上线必须回归文件集/导入地图/KN地图导入/",
-    noDir: false,
   },
-
   {
     name: "OSS - GS 存在不支持的topic",
     url: "oss://coscene-playground/A - 上线必须回归文件集/有不支持的GStopic/",
-    noDir: false,
   },
   {
     name: "OSS - CyberRT",
     url: "oss://coscene-playground/A - 上线必须回归文件集/CyberRT/",
-    noDir: false,
   },
   {
     name: "OSS - 12点的日志",
     url: "oss://coscene-playground/A - 上线必须回归文件集/12点的日志/",
-    noDir: false,
   },
   {
     name: "OSS - 未上传完成的bag",
     url: "oss://coscene-playground/A - 上线必须回归文件集/未上传完成的bag/",
-    noDir: false,
   },
   {
     name: "OSS - MCAP",
     url: "oss://coscene-playground/A - 上线必须回归文件集/MCAP/",
-    noDir: false,
   },
   {
     name: "OSS - 新的bag",
     url: "oss://coscene-playground/A - 上线必须回归文件集/需要实时更新/新的bag/",
-    noDir: false,
   },
   {
     name: "OSS - 新的bagzip",
     url: "oss://coscene-playground/A - 上线必须回归文件集/需要实时更新/新的bagzip/",
-    noDir: false,
   },
   {
     name: "OSS - 单个链接-可能会过期",
     url: "https://coscene-playground.oss-cn-hangzhou.aliyuncs.com/A%20-%20%E4%B8%8A%E7%BA%BF%E5%BF%85%E9%A1%BB%E5%9B%9E%E5%BD%92%E6%96%87%E4%BB%B6%E9%9B%86/%E9%9C%80%E8%A6%81%E5%AE%9E%E6%97%B6%E6%9B%B4%E6%96%B0/%E5%8D%95%E4%B8%AAOSS%E9%93%BE%E6%8E%A5/gen-0317089039.bag?OSSAccessKeyId=LTAI5tBnf4bCLaHoQwJ5QEXH&Expires=1e%2B30&Signature=8iahnCy8vzinIfjuJVSHfvrY9lc%3D",
-    noDir: false,
   },
   {
     name: "record - 单个链接-可能会过期",
     url: "https://tempo-prod-cn-shanghai-dogfish.oss-cn-shanghai.aliyuncs.com/2023/8/7/9/GS142-0120-UCM-T000/e2a1e114-2364-4513-9f21-0ce6f52e94d6/GS_2023-08-07-17-47-27_20.bag?Expires=1691492205&OSSAccessKeyId=LTAI5t6N9Z4oqKAoLT41oMyR&Signature=aOiOwgtxHU5Rw%2FhExCtb%2FEanMME%3D&response-content-type=application%2Foctet-stream",
-    noDir: false,
   },
 ];
 
@@ -100,12 +83,7 @@ test.beforeEach(async ({ page }) => {
 
 for (let i = 0; i < cases.length; i++) {
   test(`Upload and play bag ${cases[i].name}`, async ({ page, context }) => {
-    const currentCase = { ...cases[i], dirNames: [] };
-    currentCase["dirNames"] = currentCase.url
-      .split("oss://coscene-playground/")
-      .pop()
-      .split("/")
-      .filter((item) => item);
+    const currentCase = cases[i];
 
     test.setTimeout(timeout);
 
@@ -130,15 +108,6 @@ for (let i = 0; i < cases.length; i++) {
     await page.getByText("OK", { exact: true }).click();
 
     await page.waitForTimeout(1000);
-
-    if (!currentCase.noDir) {
-      for (let i = 0; i < currentCase.dirNames.length; i++) {
-        await page
-          .getByRole("cell", { name: currentCase.dirNames[i] })
-          .getByText(currentCase.dirNames[i])
-          .click();
-      }
-    }
 
     for (let i = 0; i < 10 * 60; i++) {
       const tdTagCount = await page.$$eval("tr", (tds) => tds.length);
